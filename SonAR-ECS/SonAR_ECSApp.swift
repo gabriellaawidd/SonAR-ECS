7f3b7448874fff3ee563a653ad79c6be95b5aa14
Gabriella Angelina Widjaja <162145755+gabriellaawidd@users.noreply.github.com>
2026-08-13 10:39:12 +0700

    Initial Commit

---
+++ SonAR-ECS/SonAR_ECSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonAR_ECSApp.swift
+//  SonAR-ECS
+//
+//  Created by Gabriella Angelina Widjaja on 13/08/26.
+//
+
+import SwiftUI
+
+@main
+struct SonAR_ECSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
